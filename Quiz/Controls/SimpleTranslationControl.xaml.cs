f85dde99961e750f4fff78b706d0aa1fc0cf93be
notjulie <rasmussenrandy189@gmail.com>
2017-01-29 13:10:01 -0600

    Added UI for adverbs.

---
+++ Quiz/Controls/SimpleTranslationControl.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Quiz.Controls
+{
+   /// <summary>
+   /// Interaction logic for SimpleTranslationControl.xaml
+   /// </summary>
+   public partial class SimpleTranslationControl : UserControl
+   {
+      /// <summary>
+      /// Initializes a new instance of class SimpleTranslationControl
+      /// </summary>
+      public SimpleTranslationControl()
+      {
+         InitializeComponent();
+
+         Loaded += SimpleTranslationControl_Loaded;
+      }
+
+      void SimpleTranslationControl_Loaded(object sender, RoutedEventArgs e)
+      {
+         MoveFocus(new TraversalRequest(FocusNavigationDirection.First));
+      }
+   }
+}
--- Quiz/MainWindow.xaml.cs
+++ Quiz/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Quiz.Controls;
 using Quiz.XmlModel;
 
 namespace Quiz
@@ -22,6 +23,8 @@ namespace Quiz
    /// </summary>
    public partial class MainWindow : Window
    {
+      #region Constructor
+
       /// <summary>
       /// Initializes a new instance of class MainWindow
       /// </summary>
@@ -33,12 +36,50 @@ namespace Quiz
          this.Loaded += MainWindow_Loaded;
       }
 
+      #endregion
+
+      #region Event Handlers
+
       void MainWindow_Loaded(object sender, RoutedEventArgs e)
       {
+         // load our language data
          PresentIndicative pi = XMLLoader.Load<PresentIndicative>("PresentIndicative.xml");
          Adverbs adverbs = XMLLoader.Load<Adverbs>("Adverbs.xml");
-         verbConjugationControl.DataContext = pi.VerbConjugations[0];
+
+         // spew up the first adverb for now
+         ShowItem(adverbs.AdverbList[0]);
       }
 
+      #endregion
+
+      #region Private Methods
+
+      private void ShowItem(object item)
+      {
+         try
+         {
+            FrameworkElement itemControl;
+
+            // create the child control according to the object type
+            switch (item.GetType().Name)
+            {
+               case "Adverb":
+                  itemControl = new SimpleTranslationControl();
+                  break;
+
+               default:
+                  throw new Exception("MainWindow.ShowItem: unsupported item type: " + item.GetType().Name);
+            }
+
+            itemControl.DataContext = item;
+            itemContainer.Content = itemControl;
+         }
+         catch (Exception e)
+         {
+            MessageBox.Show(e.Message);
+         }
+      }
+
+      #endregion
    }
 }
